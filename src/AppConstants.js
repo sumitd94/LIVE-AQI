337d470d5ccb9823acb5d830436c184415480794
Sumit Dhanania <sumitdhanania94@gmail.com>
2021-05-24 11:09:20 +0530

    Fix the insecure websocket endpoint error

--- src/AppConstants.js
+++ src/AppConstants.js
@@ -1,3 +1,3 @@
-export const SOCKET_URL = 'ws://city-ws.herokuapp.com';
+export const SOCKET_URL = 'wss://city-ws.herokuapp.com';
 export const SELECT_CITY = 'SELECT_CITY';
 export const STATIC_AQI = [45, 75, 150, 250, 350, 450];
